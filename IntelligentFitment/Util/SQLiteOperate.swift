f5b1403eb5dba696d55c44df6f8f2354480da229
gold-fish <xjyljy1022@163.com>
2016-10-20 14:31:46 +0800

    继续提交初始文件

---
+++ IntelligentFitment/Util/SQLiteOperate.swift
@@ -0,0 +1,13 @@
+//
+//  SQLiteOperate.swift
+//  IntelligentFitment
+//
+//  Created by software on 16/10/20.
+//  Copyright © 2016年 shuoren. All rights reserved.
+//
+
+import UIKit
+
+class SQLiteOperate: NSObject {
+
+}
