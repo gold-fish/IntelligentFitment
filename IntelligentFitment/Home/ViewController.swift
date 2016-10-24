125aac8deefffdaf4cdb9a88aae2f87d7aaca83d
gold-fish <xjyljy1022@163.com>
2016-10-24 15:27:59 +0800

    完成了生成二维码的功能

--- IntelligentFitment/Home/ViewController.swift
+++ IntelligentFitment/Home/ViewController.swift
@@ -4,7 +4,9 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController , AVCaptureMetadataOutputObjectsDelegate{
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate{
+    @IBOutlet weak var imgQRCode: UIImageView!
+    
     var archivePath = ""
     var captrueSession:AVCaptureSession?
     
@@ -29,59 +31,104 @@ class ViewController: UIViewController , AVCaptureMetadataOutputObjectsDelegate{
         print(model.date)
     }
     
-    @IBAction func QRCodeTapped(sender: AnyObject) {
-        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
-        //创建输入流
-        let captureInput = try? AVCaptureDeviceInput(device: captureDevice)
+    func createQRCode(content:String) -> UIImage{
+        //创建滤镜对象
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        //恢复默认设置
+        filter?.setDefaults()
         
-        captrueSession = AVCaptureSession()
+        //设置二维码数据
+        filter?.setValue(content.dataUsingEncoding(NSUTF8StringEncoding), forKey: "inputMessage")
+        //设置二维码纠错级别(L:7% M:15% Q:25% H:30% 默认值是M)
+        filter?.setValue("H", forKey: "inputCorrectionLevel")
+        //输出图像(转换成UIImage类型即可使用，只是此时图像不清晰)
+        let ciImage = filter?.outputImage
         
-        if let captureInput = captureInput{
-            captrueSession?.addInput(captureInput)
-        }
+        //再创建一个CIFalseColor滤镜，用来伪造颜色，使二维码图像更清晰，它只有三个固定的属性inputImage，inputColor0，inputColor1
+        let colorFilter = CIFilter(name: "CIFalseColor")!
+        colorFilter.setValue(ciImage, forKey: "inputImage")
         
-        //创建输出流
-        let captureOutput = AVCaptureMetadataOutput()
+        //输入颜色             
+        colorFilter.setValue(CIColor(red:0,green:0,blue:0),forKey:"inputColor0")
+        colorFilter.setValue(CIColor(red:1,green:1,blue:1),forKey:"inputColor1")
         
-        if ((captrueSession?.canAddOutput(captureOutput)) != nil){
-            captrueSession?.addOutput(captureOutput)
-            
-            captureOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
-            captureOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
-        }
-        
-        //设置采集视频区域
-        let layer = AVCaptureVideoPreviewLayer.init(session: captrueSession)
-        layer.videoGravity = AVLayerVideoGravityResizeAspectFill
-        layer.frame = CGRectMake(100, 200, 300, 300)
-        self.view.layer.addSublayer(layer)
-        
-        //开始采集视频数据
-        captrueSession?.startRunning()
+        return UIImage(CIImage: colorFilter.outputImage!.imageByApplyingTransform(CGAffineTransformMakeScale(5,5)))
     }
     
-    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!){
-        if metadataObjects == nil || metadataObjects.count == 0{
-            let alertController = UIAlertController(title: "二维码值为：", message: "暂未捕获到", preferredStyle: .Alert)
+    @IBAction func createQRCodeTapped(sender: AnyObject) {
+        imgQRCode.image = createQRCode("软件部门的二维码")
+    }
+    
+    @IBAction func QRCodeTapped(sender: AnyObject) {
+        //设置捕捉设备
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        do{
+            //创建输入流
+            let captureInput = try AVCaptureDeviceInput(device: captureDevice)
+            //创建输出流
+            let captureOutput = AVCaptureMetadataOutput()
+            captureOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+            
+            //设置会话
+            captrueSession = AVCaptureSession()
+            
+            if ((captrueSession?.canAddInput(captureInput)) != nil){
+                captrueSession?.addInput(captureInput)
+            }
+            
+            if ((captrueSession?.canAddOutput(captureOutput)) != nil){
+                captrueSession?.addOutput(captureOutput)
+            }
+            
+            //设置扫描类型(二维码)
+            captureOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+            
+            //设置采集视频区域
+            let layer = AVCaptureVideoPreviewLayer.init(session: captrueSession)
+            layer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            
+            let width = UIScreen.mainScreen().bounds.size.width - 120
+            
+            layer.frame = CGRectMake(60, 200, width, 250)
+            self.view.layer.addSublayer(layer)
+            
+            //设置提示文字
+            let showLabel = UILabel(frame: CGRectMake(75,460,260,30))
+            showLabel.text = "将二维码放入框内，即可自动扫描"
+            self.view.addSubview(showLabel)
+        }
+        catch{
+            let alertController = UIAlertController(title: "温馨提示：", message: "摄像头不可用，请在设置中开启此应用对相机的访问权限！", preferredStyle: .Alert)
             
             let cancelAction = UIAlertAction(title: "确定", style: .Cancel, handler: nil)
             alertController.addAction(cancelAction)
             
             self.presentViewController(alertController, animated: true, completion: nil)
         }
-        else{
+        
+        //开始扫描
+        captrueSession?.startRunning()
+    }
+    
+    //处理扫描结果
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!){
+        if metadataObjects != nil && metadataObjects.count > 0{
             let myMetadataObject = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
             
             if myMetadataObject.type == AVMetadataObjectTypeQRCode{
                 if let value = myMetadataObject.stringValue{
-                    let alertController = UIAlertController(title: "二维码值为：", message: value, preferredStyle: .Alert)
+                    captrueSession?.stopRunning()
+                
+                    //添加震动
+                    AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+                
+                    let alertController = UIAlertController(title: "二维码值：", message: value, preferredStyle: .Alert)
                     
                     let cancelAction = UIAlertAction(title: "确定", style: .Cancel, handler: nil)
                     alertController.addAction(cancelAction)
                     
                     self.presentViewController(alertController, animated: true, completion: nil)
-                    
-                    captrueSession?.stopRunning()
                 }
             }
         }
--- IntelligentFitment/Util/QRCodeOperate.swift
+++ IntelligentFitment/Util/QRCodeOperate.swift
@@ -3,55 +3,73 @@ import AVFoundation
 
 class QRCodeOperate: NSObject , AVCaptureMetadataOutputObjectsDelegate{
     var captrueSession:AVCaptureSession?
-    var codeValue:String?
-    var captureFlag:Bool = false
     
-    init(myView:UIView) {
+    init(myView:UIView,myViewController:UIViewController) {
         super.init()
-        
+    
+        //设置捕捉设备
         let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
-        //创建输入流
-        let captureInput = try? AVCaptureDeviceInput(device: captureDevice)
         
-        captrueSession = AVCaptureSession()
-        
-        if let captureInput = captureInput{
-            captrueSession?.addInput(captureInput)
-        }
-        
-        //创建输出流
-        let captureOutput = AVCaptureMetadataOutput()
-        
-        if ((captrueSession?.canAddOutput(captureOutput)) != nil){
-            captrueSession?.addOutput(captureOutput)
-            
+        do{
+            //创建输入流
+            let captureInput = try AVCaptureDeviceInput(device: captureDevice)
+            //创建输出流
+            let captureOutput = AVCaptureMetadataOutput()
             captureOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+            
+            //设置会话
+            captrueSession = AVCaptureSession()
+            
+            if ((captrueSession?.canAddInput(captureInput)) != nil){
+                captrueSession?.addInput(captureInput)
+            }
+            
+            if ((captrueSession?.canAddOutput(captureOutput)) != nil){
+                captrueSession?.addOutput(captureOutput)
+            }
+            
+            //设置扫描类型(二维码)
             captureOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+            
+            //设置采集视频区域
+            let layer = AVCaptureVideoPreviewLayer.init(session: captrueSession)
+            layer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            
+            let width = UIScreen.mainScreen().bounds.size.width - 120
+            
+            layer.frame = CGRectMake(60, 200, width, 250)
+            myView.layer.addSublayer(layer)
+            
+            //设置提示文字
+            let showLabel = UILabel(frame: CGRectMake(75,460,260,30))
+            showLabel.text = "将二维码放入框内，即可自动扫描"
+            myView.addSubview(showLabel)
+        }
+        catch{
+            let alertController = UIAlertController(title: "温馨提示：", message: "摄像头不可用，请在设置中开启此应用对相机的访问权限！", preferredStyle: .Alert)
+            
+            let cancelAction = UIAlertAction(title: "确定", style: .Cancel, handler: nil)
+            alertController.addAction(cancelAction)
+            
+            myViewController.presentViewController(alertController, animated: true, completion: nil)
         }
         
-        //设置采集视频区域
-        let layer = AVCaptureVideoPreviewLayer.init(session: captrueSession)
-        layer.videoGravity = AVLayerVideoGravityResizeAspectFill
-        layer.frame = CGRectMake(100, 200, 200, 200)
-        myView.layer.addSublayer(layer)
-        
-        //开始采集视频数据
+        //开始扫描
         captrueSession?.startRunning()
     }
     
     func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!){
-        if metadataObjects == nil || metadataObjects.count == 0{
-            print("未捕获到二维码！")
-        }
-        else{
+        if metadataObjects != nil && metadataObjects.count > 0{
             let myMetadataObject = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
             
             if myMetadataObject.type == AVMetadataObjectTypeQRCode{
                 if let value = myMetadataObject.stringValue{
-                    codeValue = value
-                    captureFlag = true
-                    
                     captrueSession?.stopRunning()
+                    
+                    //添加震动
+                    AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+                    //发送通知
+                    NSNotificationCenter.defaultCenter().postNotificationName("scan", object: nil, userInfo: ["codeValue":value])
                 }
             }
         }
